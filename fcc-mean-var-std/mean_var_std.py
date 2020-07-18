374dcd42cb3fa07073b16428c81be597391a4ce5
SupremeSadat <sadat.tafader@gmail.com>
2020-07-19 00:24:49 +0100

    First Project of Certification Complete

--- fcc-mean-var-std/mean_var_std.py
+++ fcc-mean-var-std/mean_var_std.py
@@ -4,5 +4,52 @@ def calculate(list):
 
 
 
+    result = {}
+    answer = []
+    try:
+        myArray = (np.asarray(list)).reshape((3,3))
+    except ValueError:
+        raise ValueError("List must contain nine numbers.")
 
-    return calculations
+    answer.append(myArray.mean(axis=0).tolist())
+    answer.append(myArray.mean(axis=1).tolist())
+    answer.append(myArray.mean().tolist())
+    result['mean']=answer
+
+    answer = []
+    myArray = (np.asarray(list)).reshape((3, 3))
+    answer.append(myArray.var(axis=0).tolist())
+    answer.append(myArray.var(axis=1).tolist())
+    answer.append(myArray.var().tolist())
+    result['variance'] = answer
+
+    answer = []
+    myArray = (np.asarray(list)).reshape((3, 3))
+    answer.append(myArray.std(axis=0).tolist())
+    answer.append(myArray.std(axis=1).tolist())
+    answer.append(myArray.std().tolist())
+    result['standard deviation'] = answer
+
+    answer = []
+    myArray = (np.asarray(list)).reshape((3, 3))
+    answer.append(myArray.max(axis=0).tolist())
+    answer.append(myArray.max(axis=1).tolist())
+    answer.append(myArray.max().tolist())
+    result['max'] = answer
+
+    answer = []
+    myArray = (np.asarray(list)).reshape((3, 3))
+    answer.append(myArray.min(axis=0).tolist())
+    answer.append(myArray.min(axis=1).tolist())
+    answer.append(myArray.min().tolist())
+    result['min'] = answer
+
+    answer = []
+    myArray = (np.asarray(list)).reshape((3, 3))
+    answer.append(myArray.sum(axis=0).tolist())
+    answer.append(myArray.sum(axis=1).tolist())
+    answer.append(myArray.sum().tolist())
+    result['sum'] = answer
+
+
+    return result
